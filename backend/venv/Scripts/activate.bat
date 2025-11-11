5adf5e3e2f06f781c78f9770510850adc5b24e54
hamiya7 <aisyamiya03@gmail.com>
2025-11-11 21:40:56 +0700

    miya
    nyoba

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\Rafi\Documents\GitHub\PrediksiDiabetes\backend\venv"
+set "VIRTUAL_ENV=D:\HAMIYA\BLENDEEEEEER!!!!\project\PrediksiDiabetes\backend\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
